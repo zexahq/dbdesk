a49d5abda351fa53b1de4ed923ffcaa668845782
Bhavik Agarwal <agarwalbhavik2001@gmail.com>
2025-11-09 15:59:28 +0530

    feat: dbdesk init

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App(): React.JSX.Element {
+  // const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+
+  return (
+    <div>
+      Hello
+    </div>
+  )
+}
+
+export default App
