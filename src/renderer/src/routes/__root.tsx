7dfef5301b47ae65d0e406e7a51b702430e40534
Anurag607 <maskeddeep@gmail.com>
2026-01-14 11:23:14 +0530

    feat: update RootLayout to conditionally render MainSidebar based on location

--- src/renderer/src/routes/__root.tsx
+++ src/renderer/src/routes/__root.tsx
@@ -1,18 +1,23 @@
-import { createRootRoute, Navigate, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Navigate, Outlet, useLocation } from '@tanstack/react-router'
 // import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import { MainSidebar } from '@renderer/components/main-sidebar'
 import { Toaster } from '@renderer/components/ui/sonner'
 
-const RootLayout = () => (
+const RootLayout = () => {
+  const location = useLocation()
+  const isConnectionList = location.pathname === '/'
+
+  return (
   <div className="flex h-screen overflow-hidden select-none">
-    <MainSidebar />
+    {!isConnectionList && <MainSidebar />}
     <div className="flex-1 h-full overflow-y-auto">
       <Outlet />
     </div>
     <Toaster position="top-right" />
     {/* <TanStackRouterDevtools position="bottom-right" /> */}
   </div>
-)
+  )
+}
 
 const NotFound = () => <Navigate to="/" replace />
 
